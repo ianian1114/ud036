3600ac052cb1187fbb024318798eda96a07975e4
ianian1114 <61895615+ianian1114@users.noreply.github.com>
2020-03-21 11:21:55 +0800

    Update break_time.py

--- break_time.py
+++ break_time.py
@@ -2,4 +2,4 @@ import webbrowser
 import time
 for loop in range(1,4):
     time.sleep(5)
-    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WXxQ")
+    webbrowser.open("https://www.youtube.com/watch?v=Ptk_1Dc2iPY")
