5559f117820846a26e715061f808fddd4fd00d21
ianian1114 <61895615+ianian1114@users.noreply.github.com>
2020-05-09 12:10:34 +0800

    Update fresh_tomatoes.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -125,6 +125,8 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <h2>{story_line}</h2>
+    <a href={movie_netflix} >Netflix連結</a>
 </div>
 '''
 
@@ -140,12 +142,21 @@ def create_movie_tiles_content(movies):
             r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
         trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
                               else None)
-
-        # Append the tile for the movie with its content filled in
+ 
+        
+    
+        # Append the title for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            story_line=movie.storyline,
+            movie_netflix=movie.TvShow_netflix
+
+            #if movie.__class__.__name__=='TvShow':
+            
+            
+            
         )
     return content
 
